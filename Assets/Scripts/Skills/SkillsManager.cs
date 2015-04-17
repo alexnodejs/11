d47f9af030f4d83e3130ee484deec551fd312bb8
Evgeny Demidchenko <promocoding@gmail.com>
2015-04-17 15:31:21 +0300

    Add stub skills

--- Assets/Scripts/Skills/SkillsManager.cs
+++ Assets/Scripts/Skills/SkillsManager.cs
@@ -28,5 +28,28 @@ public class SkillsManager : MonoBehaviour
     /// </summary>
     private void LoadStubSkills()
     {
+        skillsController.AddSkill(new SkillHero("Steals", "A_Armadura01") {
+            keyCode = KeyCode.Alpha1,
+            interval = 3.5f,
+            energyConsumption = 20.0f,
+            isAvailable = true,
+            description = "Invisible Mode",
+        });
+
+        skillsController.AddSkill(new SkillHero("Kinetic Leap", "A_Armadura03") {
+            keyCode = KeyCode.Alpha2,
+            interval = 1.0f,
+            energyConsumption = 999.0f,
+            isAvailable = false,
+            description = "Rapid Jump",
+        });
+
+        skillsController.AddSkill(new SkillHero("Hologram", "W_Machado010") {
+            keyCode = KeyCode.Alpha3,
+            interval = 10.0f,
+            energyConsumption = 10.0f,
+            isAvailable = true,
+            description = "Distracting Image",
+        });
     }
 }
