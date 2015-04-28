8d0456cdc7058356e47f7b791315ce97de3bcff8
Evgeny Demidchenko <promocoding@gmail.com>
2015-04-28 09:44:28 +0300

    Add Skill Component

---
+++ Assets/Scripts/Skills/Components/SkillComponent.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SkillComponent
+{
+    public string[] Tags;
+
+    public virtual void Apply(List<GameObject> targers)
+    {
+    }
+}
