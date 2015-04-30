7e3ba8359cee4f1eed90fd54d51db7cb99eb272b
Evgeny Demidchenko <promocoding@gmail.com>
2015-04-30 14:46:49 +0300

    Add Hero AI behaviour with active steals skill

--- Assets/Scripts/Skills/Hero/Skill/SkillSteals.cs
+++ Assets/Scripts/Skills/Hero/Skill/SkillSteals.cs
@@ -1,18 +1,22 @@
 ﻿using UnityEngine;
 using System.Collections;
+using RAIN.Entities;
 
 public class SkillSteals: SkillController
 {
-    public Mesh Mesh;
-    private VisualizationComponent visualization;
+    private GameObject hero;
     private GameObject heroBody;
+    private EntityRig aiEntity;
     private Material inactiveSkillBodyShader;
     private Material activeSkillBodyShader;
 
     void Start()
     {
-        visualization = new VisualizationComponent();
-        heroBody = GameObject.FindGameObjectWithTag(Global.Tags.heroes).transform.FindChild("Body_01").gameObject;
+        hero = GameObject.FindGameObjectWithTag(Global.Tags.heroes);
+
+        heroBody = hero.transform.FindChild("Body_01").gameObject;
+        aiEntity = hero.GetComponentInChildren<EntityRig>();
+
         inactiveSkillBodyShader = heroBody.GetComponent<Renderer>().material;
 
         activeSkillBodyShader = new Material(Shader.Find("Projector/Light"));
@@ -24,10 +28,12 @@ public class SkillSteals: SkillController
 
         if (skill.isActive)
         {
+            aiEntity.Entity.IsActive = false;
             heroBody.GetComponent<Renderer>().material = activeSkillBodyShader;
         }
         else
         {
+            aiEntity.Entity.IsActive = true;
             heroBody.GetComponent<Renderer>().material = inactiveSkillBodyShader;
         }
     }
