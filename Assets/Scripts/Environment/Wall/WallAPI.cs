f1f16016d1bf710e9d8f07d908a651a3f86d7603
unknown <andrey.vysher@gmail.com>
2015-05-22 14:06:15 +0300

    Replace Scripts folder, add Decal system to third_party_assets, add explosive wall

---
+++ Assets/Scripts/Environment/Wall/WallAPI.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using Global;
+
+public class WallAPI : MonoBehaviour 
+{
+    public GameObject exlosionPrefab;
+
+    void Awake()
+    {
+        Invoke("WallDamage", 2.7f);
+    }
+
+    public void WallDamage()
+    {
+        Invoke("DestroyThisWall", 0.1f);
+        Instantiate(exlosionPrefab, transform.position, new Quaternion(0f, 0f, 0f, 90f));  
+    }
+
+    private void DestroyThisWall()
+    {
+        Destroy(gameObject);
+    }
+}
