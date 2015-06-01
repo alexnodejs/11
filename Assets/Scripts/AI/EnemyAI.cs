60f87b6403f91406a0ffa2dd8bb4a1a1bdccb7ab
Alexander Vasileyko <alexnodejs@yahoo.com>
2015-06-01 04:41:16 +0300

    Newborn waypoint system

---
+++ Assets/Scripts/AI/EnemyAI.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class EnemyAI : MonoBehaviour {
+
+	private WaypointController _waypointController;
+	private NavMeshAgent _agent;
+
+	private Queue<Waypoint> _waypointQueue;
+	private Waypoint _currentWaypoint;
+
+
+	// Use this for initialization
+	void Start () {
+		_waypointController = GameObject.Find("Waypoint Manager").GetComponent<WaypointController>();
+		_agent = gameObject.GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// TODO: Decesion Manger
+		PatrolByPriority();
+	}
+
+	#region Future Strategy
+
+	void PatrolRandomly () {
+		if (ReachDestinition()) {
+			_currentWaypoint = _waypointController.getRandomWaypoint();
+			_agent.SetDestination(_currentWaypoint.transform.position);
+		}
+	}
+	
+	void PatrolLinearly () {
+		if (ReachDestinition()) {
+			_currentWaypoint = _waypointController.getNextWaypoint(_currentWaypoint);
+			_agent.SetDestination(_currentWaypoint.transform.position);
+		}
+	}
+
+	void PatrolByPriority () {
+		if (ReachDestinition()) {
+			if (_waypointQueue == null || _waypointQueue.Count == 0) {
+				_waypointQueue = _waypointController.getWaypointPriorityQueue();
+			}
+			
+			_currentWaypoint = _waypointQueue.Dequeue();
+			Debug.Log(_currentWaypoint.name);
+			_agent.SetDestination(_currentWaypoint.transform.position);
+		}
+	}
+
+	private bool ReachDestinition() {
+		return Vector3.Distance(transform.position, _agent.destination) < .5f;
+	}
+
+	#endregion
+}
---
+++ Assets/Scripts/AI/Waypoints/WaypointController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class WaypointController : MonoBehaviour {
+
+	public List<Waypoint> Waypoints;
+
+	/// <summary>
+	/// Gets the random waypoint.
+	/// </summary>
+	/// <returns>The random waypoint.</returns>
+	public Waypoint getRandomWaypoint() {
+		int rnd = Random.Range(0,(Waypoints.Count - 1));
+		return Waypoints[rnd];
+	}
+
+	/// <summary>
+	/// Gets the next waypoint.
+	/// </summary>
+	/// <returns>The next waypoint.</returns>
+	/// <param name="excludedWaypoint">Excluded waypoint.</param>
+	public Waypoint getNextWaypoint(Waypoint excludedWaypoint) {
+		int position = Waypoints.IndexOf(excludedWaypoint);
+
+		return Waypoints[position == -1 ? 0 : (position + 1) % Waypoints.Count];
+	}
+
+	/// <summary>
+	/// Gets the waypoint priority queue.
+	/// </summary>
+	/// <returns>The waypoint priority queue.</returns>
+	public Queue<Waypoint> getWaypointPriorityQueue() {
+		List<Waypoint> sortedWaypoints = new List<Waypoint>(Waypoints);
+
+		sortedWaypoints.Sort(delegate(Waypoint i1, Waypoint i2) { 
+			return i2.Priority.CompareTo(i1.Priority);
+		});
+
+		Debug.Log("=====>");
+
+		return new Queue<Waypoint>(sortedWaypoints);
+	}
+}
