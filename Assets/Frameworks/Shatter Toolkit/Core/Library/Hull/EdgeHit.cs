5749f077890449a78456c6ab7f5a4002256823e9
Alexander Vasileyko <vasileyko.alex@gmail.com>
2015-06-06 17:01:10 +0300

    Add only root Library folder to the .gitignore

---
+++ Assets/Frameworks/Shatter Toolkit/Core/Library/Hull/EdgeHit.cs
@@ -0,0 +1,12 @@
+// Shatter Toolkit
+// Copyright 2015 Gustav Olsson
+
+namespace ShatterToolkit
+{
+    public struct EdgeHit
+    {
+        public float scalar;
+        
+        public Edge splitA, splitB;
+    }
+}
