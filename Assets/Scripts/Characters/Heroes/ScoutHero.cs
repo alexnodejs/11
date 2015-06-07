2ac1a424004528f864a40f341bf7763836a5ee61
unknown <andrey.vysher@gmail.com>
2015-06-07 15:48:22 +0300

    Add ScoutHeroStateMachine

--- Assets/Scripts/Characters/Heroes/ScoutHero.cs
+++ Assets/Scripts/Characters/Heroes/ScoutHero.cs
@@ -1,30 +1,55 @@
 ﻿using UnityEngine;
 using System.Collections;
 using Global;
+using ICode;
 
 [AddComponentMenu("Global/Characters/ScoutHero")]
 public class ScoutHero : Hero
 {
+    public enum MoveState
+    {
+        Walk,
+        Run,
+        Duck
+    }
+
     private float walkSpeed = 2f;
     private float runSpeed = 5f;
     private bool isRunMode = false;
+    private ICodeBehaviour behavior;
+
+    protected override void Init()
+    {
+        base.Init();
+
+        behavior = gameObject.GetBehaviour();
+    }
 
     public void DuckModeToggle()
     {
         StopNavAgent();
-        CharacterAnimator.SetBool("Duck", !CharacterAnimator.GetBool("Duck"));
+
+        if (behavior.ActiveNode.Equals(behavior.stateMachine.GetNode("Duck")))
+        {
+            behavior.SetNode("Walk");
+        }
+        else
+        {
+            behavior.SetNode("Duck");
+        }
     }
 
     public void ScoutRunToggle()
     {
-        isRunMode = !isRunMode;
-        if (isRunMode)
+        StopNavAgent();
+
+        if (behavior.ActiveNode.Equals(behavior.stateMachine.GetNode("Run")))
         {
-            NavAgent.speed = runSpeed;
+            behavior.SetNode("Walk");
         }
         else
         {
-            NavAgent.speed = walkSpeed;
+            behavior.SetNode("Run");
         }
     }
 }
