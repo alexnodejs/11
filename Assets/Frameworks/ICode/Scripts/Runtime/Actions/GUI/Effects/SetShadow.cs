e7eaf6ce4456a688ed4184bedca825fdc2dac914
Alexander Vasileyko <alexnodejs@yahoo.com>
2015-06-11 23:30:45 +0300

    Return ICode back & fix shooting

---
+++ Assets/Frameworks/ICode/Scripts/Runtime/Actions/GUI/Effects/SetShadow.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+namespace ICode.Actions.UnityGUI{
+	[Category(Category.GUI)]    
+	[Tooltip("Adds or updates the shadow component.")]
+	[System.Serializable]
+	public class SetShadow : StateAction {
+		[SharedPersistent]
+		[Tooltip("GameObject to use.")]
+		public FsmGameObject gameObject;
+		[Tooltip("Effect color.")]
+		public FsmColor color;
+		[Tooltip("Effect distance.")]
+		public FsmVector2 effectDistance;
+		[Tooltip("Use graphic alpha.")]
+		public FsmBool useGraphicAlpha;
+		[Tooltip("Execute the action every frame.")]
+		public bool everyFrame;
+		
+		private Shadow component;
+		
+		public override void OnEnter ()
+		{
+			component = gameObject.Value.GetComponent<Shadow>();
+			if (component == null) {
+				component = gameObject.Value.AddComponent<Shadow>();
+			}
+			DoSetShadow ();
+			if (!everyFrame) {
+				Finish ();
+			}
+		}
+		
+		public override void OnUpdate ()
+		{
+			DoSetShadow ();
+		}
+
+		private void DoSetShadow(){
+			component.effectColor = color.Value;
+			component.effectDistance = effectDistance.Value;
+			component.useGraphicAlpha = useGraphicAlpha.Value;
+		}
+		
+	}
+}
