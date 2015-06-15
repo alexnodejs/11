66a0798bfaeff66ced8463bda440ab89fe8f177b
zerocool0686 <andrey.vysher@gmail.com>
2015-06-15 19:10:19 +0300

    Add Neutral character type, replace some methods to Hero class form Character class

--- Assets/Scripts/Characters/Heroes/Hero.cs
+++ Assets/Scripts/Characters/Heroes/Hero.cs
@@ -1,2 +1,82 @@
-﻿using UnityEngine;using System.Collections;using Global;public class Hero : Character{    public Weapon CurWeapon;    [HideInInspector] public float EnergyLevel;    [HideInInspector] public bool MovementLocked = false;    public override void TakeDamage(DamageType damageType, float damage)    {        base.TakeDamage(damageType, damage);    }    public override void SetDistinationPosition(Ray ray)    {        if (MovementLocked) return;        base.SetDistinationPosition(ray);    }    protected override void CharacterFixedUpdate()    {        base.CharacterFixedUpdate();        if (MovementLocked)        {                    }    }    public void AttackCharacter()    {
-        if (CurWeapon)            CurWeapon.Shoot();    }}
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using System.Linq;
+using Global;public class Hero : Character{    public Weapon CurWeapon;    [HideInInspector] public float EnergyLevel;    [HideInInspector] public bool MovementLocked = false;    public override void TakeDamage(DamageType damageType, float damage)    {        base.TakeDamage(damageType, damage);    }    public override void SetDistinationPosition(Ray ray)    {        if (MovementLocked) return;        base.SetDistinationPosition(ray);    }    protected override void CharacterFixedUpdate()    {        base.CharacterFixedUpdate();        if (MovementLocked)        {                    }    }    public void AttackCharacter()    {        if (CurWeapon)            CurWeapon.Shoot();    }
+
+    private void GrabObject(GameObject obj)
+    {
+        obj.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+        obj.transform.SetParent(GrabPointGameObject.transform);
+        obj.transform.localPosition = Vector3.zero;
+        obj.transform.rotation = Quaternion.Euler(Vector3.zero);
+    }
+
+    public void SelectHero()
+    {
+        if (SelectCircle != null)
+        {
+            SelectCircle.enabled = true;  // show select circle
+        }
+    }
+
+    public void UnselectHero()
+    {
+        if (SelectCircle != null)
+        {
+            SelectCircle.enabled = false;  // hide select circle
+        }
+    }
+
+    private void DropObject(GameObject obj)
+    {
+        obj.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
+        GrabPointGameObject.transform.DetachChildren();
+        ObjInHands = null;
+    }
+
+    public virtual void TryToInteract()
+    {
+        if (ObjInHands)
+        {
+            DropObject(ObjInHands);
+        }
+
+        else
+        {
+            CheckInteractableObjectsAround();
+        }
+    }
+
+    private void CheckInteractableObjectsAround()
+    {
+        Collider[] hitColliders = Physics.OverlapSphere(transform.position, InteractiveRadius);
+        List<InteractiveObject> itrObjects = new List<InteractiveObject>();
+        int i = 0;
+        while (i < hitColliders.Length)
+        {
+            InteractiveObject itrObj = hitColliders[i].GetComponent<InteractiveObject>();
+            if (itrObj)
+            {
+                itrObj.Distance = Vector3.Distance(itrObj.transform.position, transform.position);
+                itrObjects.Add(itrObj);
+            }
+
+            i++;
+        }
+
+        var sortedItrObjects = itrObjects.OrderBy(go => go.Distance).ToList();
+        sortedItrObjects[0].Interact();
+
+        if (sortedItrObjects[0] is ItrBarrel)
+        {
+            GameObject barrel = sortedItrObjects[0].Grab();
+
+            if (barrel)
+            {
+                ObjInHands = barrel;
+                GrabObject(ObjInHands);
+            }
+        }
+    }}
---
+++ Assets/Scripts/Characters/Neutrals/Neutral.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+using Global;
+
+public class Neutral : Character
+{
+    
+}
