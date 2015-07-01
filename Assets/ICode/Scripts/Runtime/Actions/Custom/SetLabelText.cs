4652e9e1b7388453e3a2fcc9f5445a1163930510
zerocool0686 <andrey.vysher@gmail.com>
2015-07-01 15:11:57 +0300

    Add HealthLevel to scout hero

--- Assets/ICode/Scripts/Runtime/Actions/Custom/SetLabelText.cs
+++ Assets/ICode/Scripts/Runtime/Actions/Custom/SetLabelText.cs
@@ -1,15 +1,47 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
+using System.Globalization;
 
-public class SetLabelText : MonoBehaviour {
+namespace ICode.Actions.Custom
+{
+    [Category(Category.Custom)]
+    [Tooltip("Set value of text.")]
+    [System.Serializable]
+    public class SetLabelText : StateAction
+    {
+        [SharedPersistent]
+        [Tooltip("GameObject to use.")]
+        public FsmGameObject gameObject;
+        [Tooltip("Image to set.")]
+        public FsmGameObject character;
+        [Tooltip("Execute the action every frame.")]
+        public bool everyFrame;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+        private Text component;
+        private float value;
+
+        public override void OnEnter()
+        {
+            value = character.Value.GetComponent<Character>().HealthLevel;
+            component = gameObject.Value.GetComponent<Text>();
+            DoSetText();
+            if (!everyFrame)
+            {
+                Finish();
+            }
+        }
+
+        public override void OnUpdate()
+        {
+            DoSetText();
+        }
+
+        private void DoSetText()
+        {
+            value = character.Value.GetComponent<Character>().HealthLevel;
+            component.text = value.ToString(CultureInfo.InvariantCulture);
+        }
+    }
 }
+
